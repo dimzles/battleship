9b3c67970162cd29cf5d82926b10242958338e66
dimzles <dimzlestv@gmail.com>
2023-06-25 18:16:37 +0100

    Add test for Gameboard creation

--- tests/battleship.test.js
+++ tests/battleship.test.js
@@ -1,27 +1,38 @@
 import { Ship } from "../src/Ship.js";
 
 describe('Ship.js tests', () => {
-    const ship = Ship(5)
+    const ship = Ship(5);
+
     test('Returns correct length', () => {
         expect(ship.getLength()).toBe(5);
-    })
+    });
+
     test('Returns correct number of hits', () => {
         expect(ship.getHits()).toBe(0);
-    })
+    });
+
     test('Returns false if ship is not sunk', () => {
         expect(ship.isSunk()).toBe(false);
-    })
+    });
 
     test('Returns true if ship is sunk', () => {
-        const sunkShip = Ship(1)
+        const sunkShip = Ship(1);
         sunkShip.hit();
-        expect(sunkShip.isSunk()).toBe(true)
-    })
+        expect(sunkShip.isSunk()).toBe(true);
+    });
 
     test('Ships can recieve hits', () => {
         const ship2 = Ship(4);
         ship2.hit();
         expect(ship2.getHits()).toBe(1);
-    })
+    });
 
+})
+
+describe('Gameboard.js tests', () => {
+    const gameboard = new Gameboard();
+
+    test('Creates a gameboard of correct size', () => {
+        expect(gameboard.board.size().toBe(100));
+    });
 })
