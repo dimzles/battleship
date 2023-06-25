6b0486678bf70a448f36072f4aa122342128a59c
dimzles <dimzlestv@gmail.com>
2023-06-25 15:44:06 +0100

    Add tests for ship factory

--- tests/battleship.test.js
+++ tests/battleship.test.js
@@ -1,11 +1,9 @@
 import testFunction from "../src/Ship.js";
 import indexFunction from "../src/index.js";
 
-describe('Test to see if project is setup correctly', () => {
-    test('Check function in Ship.js', () => {
-        expect(testFunction()).toMatch('test')
-    })
-    test('Check function in index.js', () => {
-        expect(indexFunction()).toMatch('hello world')
-    })
+describe('Ship factory returns Ship object with correct properties', () => {
+    const ship = Ship(5)
+    expect(ship.getLength()).toBe(5);
+    expect(ship.getHits()).toBe(0);
+    expect(ship.isSunk()).toBe(false);
 })
