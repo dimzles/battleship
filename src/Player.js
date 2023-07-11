7fef64b7c96253a91ad17806f9b004c81d5c4e6a
dimzles <dimzlestv@gmail.com>
2023-07-11 19:02:19 +0100

    Add playRandomMove function

--- src/Player.js
+++ src/Player.js
@@ -8,4 +8,15 @@ export default class Player {
         this.playedMoves.push(`${coordArray}`)
         gameboard.receiveAttack(coordArray)
     }
+
+    playRandomMove(gameboard) {
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
+        const coords = [x, y]
+
+        gameboard.receiveAttack(coords)
+        this.playedMoves.push(coords)
+
+        return `${coords}`
+    }
 }
